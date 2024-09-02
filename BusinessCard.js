9ad31b9c9e2c6d66b685b035fbab1202e591d88c
Custom3Dprints <marjorie.3dprints@gmail.com>
2024-09-02 17:12:11 -0400

    multiple links

--- BusinessCard.js
+++ BusinessCard.js
@@ -2,24 +2,30 @@ const mainImage = document.getElementById('image');
 const Instagram = document.getElementById('instagram');
 const Website = document.getElementById('website');
 
-function adjustLinksPosition() {
-    const imageWidth = mainImage.clientWidth;
-    const imageHeight = mainImage.clientHeight;
+
+
+function adjustPosition(elementid) {
     const containerWidth = mainImage.parentNode.clientWidth;
     const containerHeight = mainImage.parentNode.clientHeight;
+
     // Calculate the top position based on image and container height
-    const topPosition = ((containerHeight - Instagram.offsetHeight) / 2)+20;
+    const topPosition = ((containerHeight - elementid.offsetHeight) / 2)+20;
+    
     // Apply the top position to both links
-    Instagram.style.top = `${topPosition}px`;
-    Website.style.top = `${topPosition}px`;
+    elementid.style.top = `${topPosition}px`;
+    
     // Adjust font size based on container width
     const fontSize = containerWidth/18;
-    Instagram.style.fontSize = `${fontSize}px`;
-    Website.style.fontSize = `${fontSize}px`;
+    elementid.style.fontSize = `${fontSize}px`;
 }
 
-// Call the function initially
-adjustLinksPosition();
 
+// Call the function initially
+adjustPosition(Website);
 // Add event listener for window resize
-window.addEventListener('resize', adjustLinksPosition);
+window.addEventListener('resize', adjustPosition(Website));
+
+// Call the function initially
+adjustPosition(Instagram);
+// Add event listener for window resize
+window.addEventListener('resize', adjustPosition(Instagram));
